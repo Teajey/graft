d57a3ac57e642c28b6a68c0ce89e1fff0b3d1077
Teajey <21069848+Teajey@users.noreply.github.com>
2023-03-01 11:19:16 +1300

    refac: simplify entrypoints

--- packages/graft/src/main.rs
+++ packages/graft/src/main.rs
@@ -1,25 +1,8 @@
-#[cfg(not(target_arch = "wasm32"))]
-mod app;
-#[cfg(not(target_arch = "wasm32"))]
-mod cross;
-#[cfg(not(target_arch = "wasm32"))]
-mod gen;
-#[cfg(not(target_arch = "wasm32"))]
-mod graphql;
-#[cfg(not(target_arch = "wasm32"))]
-mod introspection;
-#[cfg(not(target_arch = "wasm32"))]
-mod run;
-#[cfg(not(target_arch = "wasm32"))]
-mod typescript;
-#[cfg(not(target_arch = "wasm32"))]
-mod util;
+#[cfg(target_arch = "wasm32")]
+fn main() {}
 
 #[cfg(not(target_arch = "wasm32"))]
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
-    run::run().await
+  teajey_graft::run().await
 }
-
-#[cfg(target_arch = "wasm32")]
-fn main() {}
