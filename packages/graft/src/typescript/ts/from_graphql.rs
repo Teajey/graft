cfd83386a86039c71a1261afec90cab09ae1a4e0
Teajey <21069848+Teajey@users.noreply.github.com>
2023-03-05 23:37:26 +1300

    okay, step it back a bit I'm doing things wrong

--- packages/graft/src/typescript/ts/from_graphql.rs
+++ packages/graft/src/typescript/ts/from_graphql.rs
@@ -2,10 +2,7 @@ use std::collections::HashMap;
 
 use eyre::{eyre, Report, Result};
 
-use crate::{
-    graphql::{query, schema},
-    typescript::{ts, TypescriptOptions},
-};
+use crate::{graphql::query, typescript::ts};
 
 struct TypesIndex<'t>(HashMap<String, ts::NamedType<'t>>);
 
@@ -18,14 +15,6 @@ impl<'t> TypesIndex<'t> {
     }
 }
 
-type FragmentsIndex<'t> = HashMap<String, ts::Fragment<'t>>;
-
-pub struct Context<'t> {
-    options: TypescriptOptions,
-    types: TypesIndex<'t>,
-    fragments: FragmentsIndex<'t>,
-}
-
 pub struct WithFieldedType<'t, T> {
     target: T,
     of_type: &'t ts::FieldedType<'t>,
@@ -34,34 +23,6 @@ pub struct WithFieldedType<'t, T> {
 impl<'t> ts::FieldedType<'t> {
     pub fn with<T>(&'t self, target: T) -> WithFieldedType<'t, T> {
         WithFieldedType {
-            target,
-            of_type: self,
-        }
-    }
-}
-
-pub struct WithType<'t, T> {
-    target: T,
-    of_type: &'t ts::Type<'t>,
-}
-
-impl<'t> ts::Type<'t> {
-    pub fn with<T>(&'t self, target: T) -> WithType<'t, T> {
-        WithType {
-            target,
-            of_type: self,
-        }
-    }
-}
-
-pub struct WithNamedType<'t, T> {
-    target: T,
-    of_type: &'t ts::NamedType<'t>,
-}
-
-impl<'t> ts::NamedType<'t> {
-    pub fn with<T>(&'t self, target: T) -> WithNamedType<'t, T> {
-        WithNamedType {
             target,
             of_type: self,
         }
@@ -82,57 +43,6 @@ impl<'t> TypesIndex<'t> {
     }
 }
 
-pub struct WithContext<'c, 't, T> {
-    target: T,
-    ctx: &'c Context<'t>,
-}
-
-impl<'c, 't> Context<'t> {
-    pub fn with<T>(&'c self, target: T) -> WithContext<'c, 't, T> {
-        WithContext { target, ctx: self }
-    }
-}
-
-impl<'c, 't> TryFrom<WithContext<'c, 't, query::Operation>> for ts::Operation<'t> {
-    type Error = Report;
-
-    fn try_from(value: WithContext<'c, 't, query::Operation>) -> Result<Self> {
-        todo!()
-    }
-}
-
-impl<'t> TryFrom<WithFieldedType<'t, (&'t ts::NamedType<'t>, Option<query::SelectionSet>)>>
-    for ts::NamedSelectionType<'t>
-{
-    type Error = Report;
-
-    fn try_from(
-        value: WithFieldedType<'t, (&'t ts::NamedType<'t>, Option<query::SelectionSet>)>,
-    ) -> Result<Self> {
-        let WithFieldedType {
-            target: (named, selection_set),
-            of_type,
-        } = value;
-
-        let selection_value = match named {
-            ts::NamedType::Fielded(fielded) => {
-                let Some(selection_set) = selection_set else {
-                    return Err(eyre!("Didn't provide a selection set on a type with fields."));
-                };
-                Self::SelectionSet(fielded.with(selection_set).try_into()?)
-            }
-            ts::NamedType::Leaf(_) => {
-                if selection_set.is_some() {
-                    return Err(eyre!("Selection set on a non-selectable type"));
-                }
-                Self::On(of_type)
-            }
-        };
-
-        Ok(selection_value)
-    }
-}
-
 impl<'t> TryFrom<WithFieldedType<'t, (&ts::Field<'t>, Option<query::SelectionSet>)>>
     for ts::SelectionType<'t>
 {
@@ -146,19 +56,7 @@ impl<'t> TryFrom<WithFieldedType<'t, (&ts::Field<'t>, Option<query::SelectionSet
             of_type,
         } = value;
 
-        let selection_value = match on_type.of_type {
-            ts::Type::Named(named) => {
-                ts::SelectionType::Named(of_type.with((named, selection_set)).try_into()?)
-            }
-            ts::Type::List(list) => {
-                ts::SelectionType::List(of_type.with((*list, selection_set)).try_into()?)
-            }
-            ts::Type::NonNull(non_null) => {
-                ts::SelectionType::NonNull(of_type.with((*non_null, selection_set)).try_into()?)
-            }
-        };
-
-        Ok(selection_value)
+        todo!()
     }
 }
 
@@ -234,11 +132,5 @@ impl<'t> TryFrom<WithTypesIndex<'t, query::Fragment>> for ts::Fragment<'t> {
             type_condition,
             doc: fragment,
         })
-    }
-}
-
-impl<'t> From<schema::NamedType> for ts::NamedType<'t> {
-    fn from(value: schema::NamedType) -> Self {
-        todo!()
     }
 }
